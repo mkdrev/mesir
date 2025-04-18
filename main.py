157aa2e4471c88ace17cbb4245c294d9af09f721
mkdrev <mikedeon17@gmail.com>
2025-04-18 14:38:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
+TOKEN = os.environ.get("BOT_TOKEN")  # Gunakan nama env yang aman dan jelas
+app = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+
+    await context.bot.send_photo(
+        chat_id=chat_id,
+        photo="https://i.pinimg.com/736x/99/9b/b8/999bb8d77251045efa7364b96c170509.jpg",
+        caption="Halo Bosku, selamat datang di *MESIR77!*\n\n"
+                "Pilih menu di bawah untuk mulai bermain atau cek fitur lainnya.",
+        parse_mode="Markdown"
+    )
+
+    keyboard = [
+        [InlineKeyboardButton("🎮 PLAY", web_app=WebAppInfo(url="https://jali.me/Mesir77"))],
+        [InlineKeyboardButton("🎰 RTP GACOR", web_app=WebAppInfo(url="https://jali.me/Mesir77"))],
+        [InlineKeyboardButton("🎁 PROMOTION", web_app=WebAppInfo(url="https://jali.me/Mesir77"))],
+        [InlineKeyboardButton("📲 SOCIAL MEDIA", web_app=WebAppInfo(url="https://jali.me/Mesir77"))],
+        [InlineKeyboardButton("💬 LIVECHAT", web_app=WebAppInfo(url="https://direct.lc.chat/17801934/"))],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await context.bot.send_message(
+        chat_id=chat_id,
+        text="Pilih salah satu opsi berikut:",
+        reply_markup=reply_markup
+    )
+
+if __name__ == '__main__':
+    print("[✅] Bot MESIR77 is running...")
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
